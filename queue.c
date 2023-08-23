b4e93b6555d371b422ec4b273131bc1d58f2c959
254jack <taixonj@gmail.com>
2023-08-23 18:24:46 +0300

    queue.c

---
+++ queue.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+/**
+ * queue - A function that sets the format of the data to a queue (FIFO).
+ * @head: A pointer to the stack.
+ * @num: The line number.
+ */
+void addqueue(stack_t **head, unsigned int num)
+{
+    (void)head;
+    (void)num;
+    j.lifi = 1;
+}
